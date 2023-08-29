7036d96a038b69ba1f8aa01d89b064f153bab763
somin <smhs12@naver.com>
2023-08-29 23:45:00 +0900

    style: 공통 스타일 설정

---
+++ src/style/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+export const GlobalStyles = createGlobalStyle`
+
+  *,
+  *::before,
+  *::after {
+    margin: 0; 
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Pretendard;
+  }
+  
+  body{
+    width: 500px;
+    height: 742px;
+    margin: 20px auto;
+    font-family: Pretendard;
+    display: flex;
+    justify-content: center;
+    border: 3px solid black;
+  }
+
+  ul{
+    list-style: none;
+  }
+
+  a{
+    text-decoration: none;
+    color: black;
+  }
+`;
