18150b7629e52ac40c31a012456f3cdb1654e825
somin <smhs12@naver.com>
2023-08-30 14:08:22 +0900

    feat: 이슈 상세 페이지 기능 구현

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,14 +2,25 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com/repos/facebook/react/issues';
 
+const TOKEN = process.env.REACT_APP_GIT_TOKEN;
+
+const axiosIstance = axios.create({
+  baseURL: BASE_URL,
+});
+
+axiosIstance.interceptors.request.use(config => {
+  config.headers.Authorization = `Bearer ${TOKEN}`;
+  return config;
+});
+
 export const getIssues = async (page: number) => {
-  const data = await axios.get(
+  const data = await axiosIstance.get(
     `${BASE_URL}?state=open&sort=comments&direction=desc&page=${page}&per_page=10`,
-    {
-      headers: {
-        Authorization: 'Bearer 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',
-      },
-    },
   );
+  return data.data;
+};
+
+export const getIssueDetail = async (issueNumber: number) => {
+  const data = await axiosIstance(`${BASE_URL}/${issueNumber}`);
   return data.data;
 };
--- src/pages/IssueDetail.tsx
+++ src/pages/IssueDetail.tsx
@@ -1,32 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { getIssueDetail } from 'apis';
+import { useLocation } from 'react-router';
 import { styled } from 'styled-components';
+import { IssueType } from 'types';
 
 import Layout from 'components/common/Layout';
+import Loading from 'components/common/Loading';
 import IssueItem from 'components/listItem/IssueItem';
 
 function IssueDetail() {
+  const location = useLocation();
+  const number = location.state.number;
+
+  const [issue, setIssue] = useState<IssueType | null>(null);
+
+  useEffect(() => {
+    getIssueDetail(number).then(response => setIssue(response));
+  }, [number]);
+
   return (
     <Layout>
-      <IssueDetailWrapper>
-        <DetailTitle>
-          <div>image</div>
-          <div>
-            <IssueItem />
-          </div>
-        </DetailTitle>
-        <DetailContent>
-          <p>There is currently...</p>
-          <span>ATTN: johnpapa / fillpesilva </span>
-        </DetailContent>
-        <DetailMotivation>
-          <strong>Motivation</strong>
-          <ul>
-            <li>Consistency</li>
-            <li>Debugging</li>
-          </ul>
-        </DetailMotivation>
-      </IssueDetailWrapper>
+      {issue ? (
+        <IssueDetailWrapper>
+          <DetailTitle>
+            <div>
+              <img src={`${issue.user.avatar_url}`} alt={`${issue.user.login} 이미지`} />
+            </div>
+            <div>{<IssueItem issue={issue} />}</div>
+          </DetailTitle>
+          <DetailContent>
+            <p>{issue.body}</p>
+            <span>ATTN: johnpapa / fillpesilva </span>
+          </DetailContent>
+          <DetailMotivation>
+            <strong>Motivation</strong>
+            <ul>
+              <li>Consistency</li>
+              <li>Debugging</li>
+            </ul>
+          </DetailMotivation>
+        </IssueDetailWrapper>
+      ) : (
+        <Loading />
+      )}
     </Layout>
   );
 }
@@ -34,7 +51,9 @@ function IssueDetail() {
 export default IssueDetail;
 
 const IssueDetailWrapper = styled.div`
-  padding: 20px;
+  height: 600px;
+  overflow-y: auto;
+  margin: 20px;
 `;
 
 const DetailTitle = styled.div`
@@ -46,7 +65,12 @@ const DetailTitle = styled.div`
     margin-right: 20px;
     width: 50px;
     height: 50px;
-    border: 1px solid black;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
   }
 
   div:last-child {
@@ -57,7 +81,10 @@ const DetailTitle = styled.div`
 const DetailContent = styled.div`
   margin-bottom: 16px;
   p {
+    width: 445px;
     margin-bottom: 16px;
+    word-break: break-all;
+    line-height: 1.5;
   }
 `;
 
