4a2101d4d86d1f8d60afdb8dd73282f4c4114f99
somin <smhs12@naver.com>
2023-08-30 13:55:46 +0900

    style: 로딩 컴포넌트 추가

---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { styled } from 'styled-components';
+
+function Loading() {
+  return <LoadingWrapper>데이터를 로딩중입니다.</LoadingWrapper>;
+}
+
+export default Loading;
+
+const LoadingWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
